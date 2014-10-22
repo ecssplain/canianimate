dcf9c150da99e15b9f4f69c80099741597faa2e1
Gilmore Davidson <gilmoreorless@gmail.com>
2014-10-23 08:32:35 +1100

    Analytics

--- proto/proto.js
+++ proto/proto.js
@@ -84,3 +84,14 @@
         domResults.innerHTML = html.join('');
     }
 })();
+
+// Analytics
+if (location.hostname.indexOf('canianimate.com') > -1) {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-8341018-6', 'auto');
+    ga('send', 'pageview');
+}
