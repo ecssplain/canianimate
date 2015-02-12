ff3f2d86d05b6ebefb74bbfa57e6b227f9ed1f51
Sean Curtis <scurtis@atlassian.com>
2015-02-12 13:33:39 +1100

    Fixed deploy.

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,11 @@
 echo =========================
 echo GZIP All Html, css and js
 echo =========================
-# cp -r _site _deploy
-find . -iname 'index.html' -exec gzip -n {} +
-find . -iname '404.html' -exec gzip -n {} +
-find . -iname "*.gz" -exec bash -c 'mv "$0" "${0%\.gz}"' {} \;
+cp 404.html _deploy
+cp index.html _deploy
+
+find _deploy/ -iname '*.html' -exec gzip -n {} +
+find _deploy/ -iname "*.gz" -exec bash -c 'mv "$0" "${0%\.gz}"' {} \;
 echo done.
 
 echo ==================
@@ -14,8 +15,7 @@ echo Syncing to S3
 echo ==================
 
 # sync gzipped html files
-s3cmd sync --progress --acl-public --cf-invalidate --rr --mime-type="text/html; charset=utf-8" --add-header 'Content-Encoding:gzip' --add-header 'Cache-Control: max-age=300, must-revalidate' . s3://canianimate.com/ --exclude '*.*' --include 'index.html'
-s3cmd sync --progress --acl-public --cf-invalidate --rr --mime-type="text/html; charset=utf-8" --add-header 'Content-Encoding:gzip' --add-header 'Cache-Control: max-age=300, must-revalidate' . s3://canianimate.com/ --exclude '*.*' --include '404.html'
+s3cmd sync --progress --acl-public --cf-invalidate --rr --mime-type="text/html; charset=utf-8" --add-header 'Content-Encoding:gzip' --add-header 'Cache-Control: max-age=300, must-revalidate' _deploy/ s3://canianimate.com/ --exclude '*.*' --include '*.html'
 
 # sync non gzipped, non js/css/image files
 #s3cmd sync --progress --acl-public --cf-invalidate _deploy/ s3://seancurtis.com/ --exclude '*.sh' --exclude 'assets/*' --exclude '*.html' --exclude '*.ico' --exclude '*.js' --exclude '*.css' --exclude '*.png' --exclude '*.svg'
@@ -26,10 +26,10 @@ s3cmd sync --progress --acl-public --cf-invalidate --rr --mime-type="text/html; 
 # sync all non gzipped css, js and image files to the static bucket (e.g. images)
 #s3cmd sync --progress --acl-public --cf-invalidate --add-header 'Cache-Control: max-age=86400' _deploy/ s3://seancurtis.com/ --exclude '*.*'  --include '*.png'
 
-# echo ==================
-# echo Cleaning up _deploy
-# echo ==================
+echo ==================
+echo Cleaning up _deploy
+echo ==================
 
-# rm -rf _deploy
+rm -rf _deploy
 
 exit 0
